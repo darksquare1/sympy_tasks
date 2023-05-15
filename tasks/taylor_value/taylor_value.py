171f9684411a1e82735d6d865255382ef395c02e
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-05-15 12:20:59 +0300

    Add files via upload

---
+++ tasks/taylor_value/taylor_value.py
@@ -0,0 +1,12 @@
+import sympy as sp
+
+x = sp.Symbol('x')
+f = sp.simplify(input()) # наша функция
+n = int(input()) # до какого члена разбить ряд
+a = int(input()) # точка в которой ищем приближенное значение
+# найдем ряд тейлора функции f(x) в точке 0
+taylor_series = sp.series(f, x, 0, n=n).removeO()
+print('Ряд', taylor_series)
+# найдем приближенное значение
+taylor_series = taylor_series.subs(x, a)
+print('Приближенное значение', taylor_series.evalf().round(3))
