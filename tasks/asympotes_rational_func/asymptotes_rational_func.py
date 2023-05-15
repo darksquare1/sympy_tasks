d3ace4bb9af8e046ff4e93ec77885fe5eceb14e3
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-05-15 12:21:55 +0300

    Add files via upload

---
+++ tasks/asympotes_rational_func/asymptotes_rational_func.py
@@ -0,0 +1,24 @@
+import sympy as sp
+
+
+def n_p_inf(q):  # +- бесконечность
+    return q == sp.oo or q == -sp.oo
+
+
+x = sp.Symbol('x')
+
+f = sp.simplify(input())  # наша функция
+denom = f.as_numer_denom()[1]  # знаменатель
+denom_zeros = sp.solve(denom, x)  # ищем точки бесконечного разрыва
+vertical_asymptotes = []
+for i in denom_zeros:
+    # если один из пределов слева или справа в точке разрыва бесконечен то i является вертикальной ассимптотой
+    if n_p_inf(sp.limit(f, x, i, '-')) or n_p_inf(sp.limit(f, x, i, '-')):
+        vertical_asymptotes.append(i)
+k = sp.limit(f / x, x, sp.oo)
+b = sp.limit(f - k * x, x, sp.oo)
+print('Вертикальные ассимптоты', end=": ")
+
+ans = [f'x = {i}' for i in sorted(vertical_asymptotes)]
+print(', '.join(ans) if ans else 'отсуствуют')
+print('Горизонтальная ассимптота:', f'y = {k * x + b}')
