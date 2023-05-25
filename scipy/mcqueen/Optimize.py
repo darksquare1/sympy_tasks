19d8a4ccc0f67fe0352c2181bd4197f7708b6d9b
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-05-25 18:43:27 +0300

    Add files via upload

---
+++ scipy/mcqueen/Optimize.py
@@ -0,0 +1,9 @@
+from scipy.optimize import minimize
+from scipy.optimize import rosen
+m, n = map(int, input().split())
+s = 0
+for i in range(m):
+    xi = [float(i) for i in input().split()] # считываем значение точки
+    b = minimize(rosen, xi, method='Nelder-Mead') # находим ближайшую точку минимума
+    s += b.fun # суммируем значения в этих точках
+print(s.round())
