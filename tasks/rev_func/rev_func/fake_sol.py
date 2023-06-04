d97dd6192d44ba2360133744170b0f6572c6066e
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-06-04 19:04:35 +1000

    Add files via upload

---
+++ tasks/rev_func/rev_func/fake_sol.py
@@ -0,0 +1,19 @@
+#import sympy as sp
+#from matplotlib import pyplot as plt
+#import numpy as np
+
+f = input()
+#x, y = sp.symbols("x y")
+#a = sp.Eq(y, sp.simplify(f))
+#a = sp.Eq(a.lhs.subs(y, x), a.rhs.subs(x, y))
+#q = sp.solve(a, y)
+#l = [sp.Eq(y, i).rhs for i in q]
+#print(*l)
+#m = np.linspace(-10, 10, 1000)
+#for i in l:
+ #   y = i
+  #  lambd_expr = sp.lambdify(x, y)
+   # output = lambd_expr(m)
+    #plt.plot(m, output)
+#plt.show()
+print('sqrt(x)')
