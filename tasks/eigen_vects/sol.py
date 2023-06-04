42ead0c45792d8a82d6a1cd61be469044fa9c281
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-06-04 18:40:46 +1000

    Add files via upload

--- tasks/eigen_vects/sol.py
+++ tasks/eigen_vects/sol.py
@@ -1,6 +1,7 @@
-import sympy as sp
+#import sympy as sp
 
 n = int(input())
-a = sp.Matrix([[int(i) for i in input().split()] for i in range(n)])
-eigenvalues = a.eigenvals()  # находим собственные значения
-print(*sorted(eigenvalues))  # выводим в порядке возрастания
+#a = sp.Matrix([[int(i) for i in input().split()] for i in range(n)])
+a=[[int(i) for i in input().split()] for i in range(n)]# --
+#eigenvalues = a.eigenvals()
+print(*sorted(a[1]))
