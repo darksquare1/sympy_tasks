968491ca48805d2d79d8ffbf5af24f2f83f64279
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-06-06 16:38:51 +0300

    Update extrems.py

--- tasks/extrems/extrems.py
+++ tasks/extrems/extrems.py
@@ -4,14 +4,13 @@ x = sp.Symbol('x')
 f = sp.simplify(input())
 df = sp.diff(f, x)
 roots = sp.solve(df, x)
-ans = []
-
+flag = False
 for root in roots:
     if sp.diff(df, x).subs(x, root) > 0:
-        ans.append(f"Точка ({root}, {f.subs(x, root)}) - локальный минимум")
+        print(f"Точка ({root}, {f.subs(x, root)}) - локальный минимум")
+        flag = True
     elif sp.diff(df, x).subs(x, root) < 0:
-        ans.append(f"Точка ({root}, {f.subs(x, root)}) - локальный максимум")
-if ans:
-    [print(i) for i in ans]
-else:
+        print(f"Точка ({root}, {f.subs(x, root)}) - локальный максимум")
+        flag = True
+if not (flag):
     print("Функция не имеет экстремумов")
