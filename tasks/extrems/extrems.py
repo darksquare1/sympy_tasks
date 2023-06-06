988fb40d92e140aa8296f0d6e0bcf7f651c03e00
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-06-06 16:14:03 +0300

    Update extrems.py

--- tasks/extrems/extrems.py
+++ tasks/extrems/extrems.py
@@ -1,21 +1,17 @@
-
 import sympy as sp
-# Определяем переменную x
+
 x = sp.Symbol('x')
-# Определяем функцию f(x)
 f = sp.simplify(input())
-# Находим производную функции
 df = sp.diff(f, x)
-# Находим корни производной
 roots = sp.solve(df, x)
-# Определяем тип экстремумов
-if len(roots) == 0:
-    print("Функция не имеет экстремумов")
+ans = []
+
+for root in roots:
+    if sp.diff(df, x).subs(x, root) > 0:
+        ans.append(f"Точка ({root}, {f.subs(x, root)}) - локальный минимум")
+    elif sp.diff(df, x).subs(x, root) < 0:
+        ans.append(f"Точка ({root}, {f.subs(x, root)}) - локальный максимум")
+if ans:
+    [print(i) for i in ans]
 else:
-    for root in roots:
-        if sp.diff(df, x).subs(x, root) > 0:
-            print(f"Точка ({root}, {f.subs(x, root)}) - локальный минимум")
-        elif sp.diff(df, x).subs(x, root) < 0:
-            print(f"Точка ({root}, {f.subs(x, root)}) - локальный максимум")
-        else:
-            print(f"Точка ({root}, {f.subs(x, root)}) - не является экстремумом")
+    print("Функция не имеет экстремумов")
