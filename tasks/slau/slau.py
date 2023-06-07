cc1f1ad3046ec1c596270931af9527ccb7358b95
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-06-07 11:20:31 +0300

    Update slau.py

--- tasks/slau/slau.py
+++ tasks/slau/slau.py
@@ -6,6 +6,6 @@ symbols = sp.symbols(f'x:{m}')
 equations = []
 for i in range(n):
     expr = input()
-    expr = sp.simplify(expr[:expr.find('=')] + str(-int(expr[expr.find('=') + 1]))) # приводим к однородной
+    expr = sp.simplify(expr[:expr.find('=')] + '-' + str(int(expr[expr.find('=') + 1:])))  # приводим к однородной
     equations.append(expr)
-print('решение системы:', sp.solve(equations, symbols))
+print('system solution:', sp.solve(equations, symbols) if sp.solve(equations, symbols) else "No solution")
