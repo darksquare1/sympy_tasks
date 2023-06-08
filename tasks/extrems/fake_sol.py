e1456c267be852e024027a314e93520c54123f9d
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-06-08 16:20:12 +1000

    Add files via upload

---
+++ tasks/extrems/fake_sol.py
@@ -0,0 +1,18 @@
+#import sympy as sp
+
+#x = sp.Symbol('x')
+#f = sp.simplify(input())
+f=input()#--
+#df = sp.diff(f, x)
+#roots = sp.solve(df, x)
+#flag = False
+#for root in roots:
+    #if sp.diff(df, x).subs(x, root) > 0:
+        #print(f"({root}, {f.subs(x, root)}) - min")
+        #flag = True
+    #elif sp.diff(df, x).subs(x, root) < 0:
+        #print(f"({root}, {f.subs(x, root)}) - max")
+        #flag = True
+#if not (flag):
+    #print("No extremums")
+print('No extremums')#--
