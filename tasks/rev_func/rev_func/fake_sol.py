1319de57af47b9518eb72c6b88964f94039dcb73
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-06-08 09:12:56 +0300

    Update fake_sol.py

--- tasks/rev_func/rev_func/fake_sol.py
+++ tasks/rev_func/rev_func/fake_sol.py
@@ -1,5 +1,5 @@
 #import sympy as sp
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 #import numpy as np
 
 f = input()
@@ -8,12 +8,13 @@ f = input()
 #a = sp.Eq(a.lhs.subs(y, x), a.rhs.subs(x, y))
 #q = sp.solve(a, y)
 #l = [sp.Eq(y, i).rhs for i in q]
-#print(*l)
-#m = np.linspace(-10, 10, 1000)
+#m = np.linspace(-10, 10, 10000)
+
 #for i in l:
- #   y = i
-  #  lambd_expr = sp.lambdify(x, y)
-   # output = lambd_expr(m)
-    #plt.plot(m, output)
-#plt.show()
-print('sqrt(x)')
+   # y = i
+    #lambd_expr = sp.lambdify(x, y)
+    #output = lambd_expr(m)
+    #plt.plot(m, output,'green')
+plt.plot(10,5)# --
+plt.grid()
+plt.savefig('output.png')
