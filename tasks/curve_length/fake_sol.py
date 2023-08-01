90d4dc3aa05e211f5302f76f775fc5f7e2c54aee
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-08-01 17:48:00 +0900

    Update fake_sol.py

--- tasks/curve_length/fake_sol.py
+++ tasks/curve_length/fake_sol.py
@@ -1,10 +1,8 @@
-#import sympy as sp
-
-#x = sp.Symbol('x')
-#f = sp.simplify(input())
-f=input()
+import sympy as sp
+x = sp.Symbol('x')
+f = sp.simplify(input())
 a, b = map(int, input().split())
-#dif_f = f.diff(x)
-#f = sp.sqrt(1 + dif_f ** 2)  
-#ans = sp.integrate(f, (x, a, b))  
+dif_f = f.diff(x)
+f = sp.sqrt(1 + dif_f ** 2)  
+ans = sp.integrate(f, (x, a, b))  
 print(100)
