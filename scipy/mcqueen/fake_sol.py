12b323c6a11c6d09d3cb8e70795d199916e866d1
darksquare1 <132488469+darksquare1@users.noreply.github.com>
2023-08-01 16:28:44 +0900

    Update fake_sol.py

--- scipy/mcqueen/fake_sol.py
+++ scipy/mcqueen/fake_sol.py
@@ -1,9 +1,9 @@
-#from scipy.optimize import minimize
-#from scipy.optimize import rosen
+from scipy.optimize import minimize
+from scipy.optimize import rosen
 m, n = map(int, input().split())
 s = 0
 for i in range(m):
     xi = [float(i) for i in input().split()]
-    #b = minimize(rosen, xi, method='Nelder-Mead')
-    #s += b.fun
+    b = minimize(rosen, xi, method='Nelder-Mead')
+    s += b.fun
 print(round(s))
